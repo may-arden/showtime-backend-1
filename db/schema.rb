f2a895d3d6df2771391e5fa649940e8d20c7e1ba
may arden <mayard3n@gmail.com>
2021-10-15 12:17:00 -0700

    rollback migration to create tables with correct timing re:associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_15_190838) do
+ActiveRecord::Schema.define(version: 2021_10_15_190831) do
 
   create_table "days", force: :cascade do |t|
     t.string "name"
@@ -18,14 +18,6 @@ ActiveRecord::Schema.define(version: 2021_10_15_190838) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_days_on_user_id"
-  end
-
-  create_table "exercises", force: :cascade do |t|
-    t.string "name"
-    t.string "category"
-    t.string "instructions"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "hours", force: :cascade do |t|
